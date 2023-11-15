ac0fb9fa7dc886164070d7de58da410075ac842f
DESKTOP-PKK6FUL\Kapral Klitorchuk <vitchenko.kirill@gmail.com>
2023-11-15 23:09:07 +0200

    add s3 provider

--- config/plugins.js
+++ config/plugins.js
@@ -4,4 +4,18 @@ module.exports = ({ env }) => ({
       defaultDepth: 3,
     },
   },
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        region: env("AWS_REGION"),
+        params: {
+          ACL: env("AWS_ACL", "public-read"),
+          Bucket: env("AWS_BUCKET"),
+        },
+      },
+    },
+  },
 });
